61bb5488f48d5e62d268576367f11f671d5bbab2
Mohammadali <malikakavand@gmail.com>
2023-08-04 23:04:28 +0330

    add functions to Calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,16 @@
 package calculator;
 
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+    public int multiply(int a, int b) {
+        return a * b;
     }
+
+    public double divide(int a, int b) {
+        return (double) a / b;
+    }
+
+    public double power(int a, int b) {
+        return Math.pow(a, b);
+    }
+
 }
