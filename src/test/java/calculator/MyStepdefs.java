f54b3507bb827aac0849251c4ec37b94af389f7e
Mohammadali <malikakavand@gmail.com>
2023-08-04 23:04:07 +0330

    add stepdefs for new operators

--- src/test/java/calculator/MyStepdefs.java
+++ src/test/java/calculator/MyStepdefs.java
@@ -10,29 +10,37 @@ public class MyStepdefs {
     private Calculator calculator;
     private int value1;
     private int value2;
-    private int result;
+
+    private String operator;
+
+    private double result;
 
     @Before
     public void before() {
         calculator = new Calculator();
     }
 
-    @Given("^Two input values, (-?\\d+) and (-?\\d+)$")
-    public void twoInputValuesAnd(int arg0, int arg1) {
+    @Given("^Three input values, (-?\\d+) and (-?\\d+) and ([/^*])$")
+    public void threeInputValuesFirstAndSecondAndOperator(int arg0, int arg1, String arg2) {
         value1 = arg0;
         value2 = arg1;
+        operator = arg2;
     }
 
-
-    @When("^I add the two values$")
-    public void iAddTheTwoValues() {
-        result = calculator.add(value1, value2);
-        System.out.print(result);
-    }
-
-    @Then("^I expect the result (\\d+)$")
-    public void iExpectTheResult(int arg0) {
-        Assert.assertEquals(arg0, result);
+    @When("^I want to the result$")
+    public void iWantToTheResult() {
+        if (operator.equalsIgnoreCase("*"))
+            result = calculator.multiply(value1, value2);
+        else if (operator.equalsIgnoreCase("/"))
+            result = calculator.divide(value1, value2);
+        else result = calculator.power(value1, value2);
 
     }
+
+    @Then("^I expect the result (.+)$")
+    public void iExpectTheResultResult(String arg0) {
+        double num = Double.parseDouble(arg0);
+        Assert.assertEquals(num, result, 0.001);
+    }
+
 }
